61bc6bab12176ca7aaa485ecc688cb604643f820
Tiqur <55461688+Tiqur@users.noreply.github.com>
2020-04-29 22:45:38 -0700

    Added various classes and enums

---
+++ src/RayTracing/Objects/Sphere.rs
@@ -0,0 +1,14 @@
+use crate::Classes::Vec3::Vec3;
+use crate::RayTracing::Traits::Shape::Shape;
+use crate::Classes::Rgb::Rgb;
+
+pub struct Sphere {
+    pub pos: Vec3,
+    pub radius: f32
+}
+
+impl Shape for Sphere {
+    fn intersection(&self) -> f32 {
+        unimplemented!()
+    }
+} 
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,42 @@ use minifb::WindowOptions;
 mod Classes;
 use Classes::Vec3::Vec3;
 use Classes::Rgb::Rgb;
+use Classes::Point2D::Point2D;
+
+mod RayTracing;
+use RayTracing::Ray::Ray;
+use RayTracing::Camera::Camera;
+use RayTracing::Traits::Shape::Shape;
+use RayTracing::Scene::Scene;
+
+use std::time::*;
+use std::thread;
 
 fn main() {
     let width = 1200;
     let height = 800;
-    let mut window = Classes::Window::Window{
+    // let threads = 2;
+    let mut window = Classes::Window::Window {
         width,
         height,
         primary_buffer: vec![0; width * height],
         secondary_buffer: vec![0; width * height]
     };
+    
+    
+    let scene = Scene {
+        camera: Camera {
+            pos: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }
+        },
+        objects: vec![]
+    };
+
+    scene.render(&mut window);
+
     let mut minifb_window = minifb::Window::new(
         "RayTracer",
         width,
@@ -24,30 +50,10 @@ fn main() {
             panic!("{}", e)
         });
 
+
+
+
     while minifb_window.is_open() && !minifb_window.is_key_down(minifb::Key::Escape) {
-
-        let mut index = 0;
-        for i in 0..window.height {
-            for x in 0..window.width {
-                let color1 = Rgb {
-                    r: 255.0,
-                    g: 255.0,
-                    b: 255.0
-                };
-                let color2 = Rgb {
-                    r: 120.0,
-                    g: 120.0,
-                    b: 255.0
-                };
-                let mixed = color1.mix(color2, i as f32 / window.height as f32);
-
-                window.secondary_buffer[index] = mixed.to_int();
-                index += 1;
-            }
-        }
-
-
-
         window.swap_buffers();
         minifb_window.update_with_buffer(&window.primary_buffer, width, height)
             .unwrap();
